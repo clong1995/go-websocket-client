0e28e5d1c4e26176e0c98f6836138b0d45fff54d
huazhimeng <2902151499@qq.com>
2025-02-24 23:47:33 +0800

    提交

--- send.go
+++ send.go
@@ -1,19 +1,10 @@
 package websocket
 
-import "log"
-
 func Send(msg Message) (err error) {
-	if err = conn.WriteJSON(msg); err != nil {
+	messageQueue <- msg
+	/*if err = conn.WriteJSON(msg); err != nil {
 		log.Println(err)
 		return
-	}
+	}*/
 	return
-}
-
-// Message 只传递动作信息，不得传递其他数据
-type Message struct {
-	Subject string  `json:"s"`
-	From    int64   //消息的来源用户,这个值不是发送方设置的，是注册成功后主动设置的。
-	Target  []int64 `json:"t"` //消息的目的用户
-	Payload string  `json:"p"`
 }
--- websocket.go
+++ websocket.go
@@ -12,53 +12,62 @@ import (
 
 var wsClose = make(chan bool)
 
+// var ir = &isRun{}
+var ws string
+
 func init() {
-	go run()
+	ws = config.Value("WEBSOCKET")
+	run()
 }
 
 func run() {
-	if <-wsClose {
-		return
-	}
 	//连接
 	conn := connect()
 	//监听消息
-	listen(conn)
+	go listen(conn)
 }
 
 func listen(conn *websocket.Conn) {
 	for {
-		var msg Message
-		err := conn.ReadJSON(&msg)
-		if err != nil {
-			log.Println(err)
-			var syntaxError *json.SyntaxError
-			if errors.As(err, &syntaxError) { //JSON解析错误
-				continue
-			}
+		select {
+		case <-wsClose:
 			_ = conn.Close()
-			go run()
+			log.Println("websocket closed")
 			return
-		}
+		default:
+			var msg Message
+			err := conn.ReadJSON(&msg)
+			if err != nil {
+				log.Println(err)
+				var syntaxError *json.SyntaxError
+				if errors.As(err, &syntaxError) { //JSON解析错误
+					continue
+				}
+				_ = conn.Close()
+				log.Println("reconnecting...")
+				time.Sleep(time.Second)
+				run()
+				return
+			}
 
-		//TODO 处理收到的消息
+			//TODO 处理收到的消息
+		}
 	}
 }
 
 func connect() (conn *websocket.Conn) {
 	var err error
-	ws := config.Value("WEBSOCKET")
 	for {
 		headers := http.Header{}
 		headers.Add("a", "xxx") //这里时间要冲新生成
-		if conn, _, err = websocket.DefaultDialer.Dial(ws, headers); err == nil {
+		if conn, _, err = websocket.DefaultDialer.Dial(ws, headers); err != nil {
 			log.Println(err)
 			log.Println("Connection lost, reconnecting")
+			time.Sleep(1 * time.Second)
 		} else {
 			log.Println("Connection connected")
 			break
 		}
-		time.Sleep(1 * time.Second)
 	}
 	return
 }
